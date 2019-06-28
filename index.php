bf4c71a215640858addf06e6bd77ce37ff66af8c
Marcelo <msouza7681@gmail.com>
2019-06-28 12:54:01 -0300

    add hcodebr classes

--- index.php
+++ index.php
@@ -1,5 +1,21 @@
 <?php 
 
-echo "OK";
+require_once("vendor/autoload.php");
 
-?>
+$app = new \Slim\Slim();
+
+$app->config('debug', true);
+
+$app->get('/', function() {
+	
+	$sql = new Hcode\DB\Sql();
+	
+	$result = $sql->select("SELECT * FROM tb_users");
+
+	echo json_encode($result);
+
+});
+
+$app->run();
+
+ ?>
---
+++ vendor/hcodebr/php-classes/src/DB/Sql.php
@@ -0,0 +1,76 @@
+<?php 
+
+namespace Hcode\DB;
+
+class Sql {
+
+	const HOSTNAME = "127.0.0.1";
+	const USERNAME = "root";
+	const PASSWORD = "";
+	const DBNAME = "db_ecommerce";
+
+	private $conn;
+
+	public function __construct()
+	{
+
+		$this->conn = new \PDO(
+			"mysql:dbname=".Sql::DBNAME.";host=".Sql::HOSTNAME, 
+			Sql::USERNAME,
+			Sql::PASSWORD
+		);
+
+	}
+
+	private function setParams($statement, $parameters = array())
+	{
+
+		foreach ($parameters as $key => $value) {
+			
+			$this->bindParam($statement, $key, $value);
+
+		}
+
+	}
+
+	private function bindParam($statement, $key, $value)
+	{
+
+		$statement->bindParam($key, $value);
+
+	}
+
+	/**
+	 * apenas executa uma query no banco
+	 */
+	public function query($rawQuery, $params = array())
+	{
+
+		$stmt = $this->conn->prepare($rawQuery);
+
+		$this->setParams($stmt, $params);
+
+		$stmt->execute();
+
+	}
+
+
+	/**
+	 * executa um query no banco e returna um array
+	 */
+	public function select($rawQuery, $params = array()):array
+	{
+
+		$stmt = $this->conn->prepare($rawQuery);
+
+		$this->setParams($stmt, $params);
+
+		$stmt->execute();
+
+		return $stmt->fetchAll(\PDO::FETCH_ASSOC);
+
+	}
+
+}
+
+ ?>
